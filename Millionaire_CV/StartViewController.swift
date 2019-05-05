105552d001b4fcba892b4d0b9bba3b806629d121
Ihor YERIN <ierinigor@gmail.com>
2019-05-05 18:40:58 +0300

    2nd

--- Millionaire_CV/StartViewController.swift
+++ Millionaire_CV/StartViewController.swift
@@ -12,9 +12,14 @@ class StartViewController: UIViewController {
 
     
     @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let whiteColor : UIColor = UIColor( red: 1, green: 1, blue: 1, alpha: 1.0 )
+        startButton.layer.borderColor = whiteColor.cgColor
+        startButton.layer.borderWidth = 1.0
+        startButton.layer.cornerRadius = 15
         helloLabel.text = "Привет! Меня зовут Игорь Ерин и сегодня я хочу сыграть с тобой в игру) В процессе игры ты узнаешь, что происходило со мной от рождения до сегодняшнего дня, и почему мое приложение сейчас на этом экране. Enjoy!"
     }
 
--- Millionaire_CV/ViewController.swift
+++ Millionaire_CV/ViewController.swift
@@ -10,16 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var questions = MyData.shared.questions
-    var qNbr = 0
-    var fiftyIsUsed = 0
-    @IBOutlet weak var btnA: UIButton!
-    @IBOutlet weak var btnB: UIButton!
-    @IBOutlet weak var btnC: UIButton!
-    @IBOutlet weak var btnD: UIButton!
-    @IBOutlet weak var questionText: UILabel!
+    // MARK: - Properties
+    private let questions = MyData.shared.questions
+    private var qNbr = 0
+    private var fiftyIsUsed = 0
+        // private var isFiftyUsed = false
+    var answerButtons: [UIButton] {
+        return [btnA, btnB, btnC, btnD]
+    }
+    var isSimulator: Bool {
+        #if (arch(i386) || arch(x86_64)) && os(iOS)
+            return true
+        #else
+            return false
+        #endif
+    }
     
-    @IBAction func buttons(_ sender: UIButton) {
+    // MARK: -
+    // MARK: - Outlets
+    @IBOutlet private weak var btnA: UIButton!
+    @IBOutlet private weak var btnB: UIButton!
+    @IBOutlet private weak var btnC: UIButton!
+    @IBOutlet private weak var btnD: UIButton!
+    @IBOutlet private weak var questionText: UILabel!
+    @IBOutlet weak var afterAnswerView: UIView!
+    @IBOutlet weak var correctLabel: UILabel!
+    @IBOutlet weak var hintLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var fiftyFifty: UIButton!
+    @IBOutlet weak var AskView: UIView!
+    @IBOutlet weak var pbA: UIProgressView!
+    @IBOutlet weak var pbB: UIProgressView!
+    @IBOutlet weak var pbC: UIProgressView!
+    @IBOutlet weak var pbD: UIProgressView!
+    @IBOutlet weak var lblA: UILabel!
+    @IBOutlet weak var lblB: UILabel!
+    @IBOutlet weak var lblC: UILabel!
+    @IBOutlet weak var lblD: UILabel!
+    @IBOutlet weak var askAudience: UIButton!
+    
+    // MARK: -
+    // MARK: - Buttons
+    @IBAction private func buttons(_ sender: UIButton) {
         if sender.tag == questions[qNbr].correctAns {
             nextQuestion(qNbr: qNbr, tag: sender.tag)
         } else {
@@ -27,10 +59,6 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var afterAnswerView: UIView!
-    @IBOutlet weak var correctLabel: UILabel!
-    @IBOutlet weak var hintLabel: UILabel!
-    @IBOutlet weak var nextButton: UIButton!
     @IBAction func nextButton(_ sender: UIButton) {
         if (nextButton.currentTitle!) == "Продолжить" {
             afterAnswerView.isHidden = true
@@ -45,7 +73,6 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var fiftyFifty: UIButton!
     @IBAction func fiftyFifty(_ sender: UIButton) {
         fiftyFifty.isEnabled = false
         var arr = [btnA, btnB, btnC, btnD]
@@ -59,32 +86,52 @@ class ViewController: UIViewController {
         fiftyIsUsed = 1
     }
     
-    @IBOutlet weak var AskView: UIView!
-    @IBOutlet weak var pbA: UIProgressView!
-    @IBOutlet weak var pbB: UIProgressView!
-    @IBOutlet weak var pbC: UIProgressView!
-    @IBOutlet weak var pbD: UIProgressView!
-    @IBOutlet weak var lblA: UILabel!
-    @IBOutlet weak var lblB: UILabel!
-    @IBOutlet weak var lblC: UILabel!
-    @IBOutlet weak var lblD: UILabel!
     @IBAction func askButton(_ sender: UIButton) {
         AskView.isHidden = true
     }
-    @IBOutlet weak var askAudience: UIButton!
+    
+    @IBAction func askAudience(_ sender: UIButton) {
+        if fiftyIsUsed == 1 {
+            askForTwo()
+        } else {
+            askForFour()
+        }
+        askAudience.isEnabled = false
+    }
+    
+    @IBAction func callFriend(_ sender: UIButton) {
+        
+        //if
+        //let cleanPhoneNumber = phone.components(separatedBy: CharacterSet.decimalDigits.inverted).joined(separator: "")
+        //        let urlString:String = "tel://+380963144622"
+        //
+        //        if let phoneCallURL = URL(string: urlString) {
+        //            if (UIApplication.shared.canOpenURL(phoneCallURL)) {
+        //                UIApplication.shared.open(phoneCallURL, options: [:], completionHandler: nil)
+        //            }
+        //        }
+        
+        if isSimulator {
+            print(1)
+        } else {
+            print(2)
+        }
+    }
+
+    // MARK: -
+    // MARK: - Methods
     
     func askForTwo() {
-        let btnArr = [btnA, btnB, btnC, btnD]
         let lblArr = [lblA, lblB, lblC, lblD]
         var pbArr = [pbA, pbB, pbC, pbD]
         let randomVoteA = Int(arc4random_uniform(UInt32(60)) + UInt32(40))
         let randomVoteD = (100 - randomVoteA)
         for i in 0...3 {
-            if btnArr[i]?.isEnabled == false {
+            if answerButtons[i].isEnabled == false {
                 lblArr[i]?.isHidden = true
                 pbArr[i]?.isHidden = true
             } else {
-                if btnArr[i]?.tag == questions[qNbr].correctAns {
+                if answerButtons[i].tag == questions[qNbr].correctAns {
                     pbArr[i]?.progress = Float(randomVoteA)/100
                     lblArr[i]?.text = "\(Int((pbArr[i]?.progress)! * 100))%"
                 } else {
@@ -99,6 +146,7 @@ class ViewController: UIViewController {
         lblD.text = "D: " + lblD.text!
         AskView.isHidden = false
     }
+    
     func askForFour() {
         let randomVoteA = Int(arc4random_uniform(UInt32(50)) + UInt32(35))
         let randomVoteB = Int(arc4random_uniform(UInt32(100 - randomVoteA)))
@@ -119,85 +167,37 @@ class ViewController: UIViewController {
         lblD.text = "D:  \(Int(pbD.progress * 100))%"
         AskView.isHidden = false
     }
-    @IBAction func askAudience(_ sender: UIButton) {
-        if fiftyIsUsed == 1 {
-            askForTwo()
-        } else {
-            askForFour()
-        }
-       askAudience.isEnabled = false
-    }
 
-    @IBAction func callFriend(_ sender: UIButton) {
-
-        //let cleanPhoneNumber = phone.components(separatedBy: CharacterSet.decimalDigits.inverted).joined(separator: "")
-//        let urlString:String = "tel://+380963144622"
-//
-//        if let phoneCallURL = URL(string: urlString) {
-//            if (UIApplication.shared.canOpenURL(phoneCallURL)) {
-//                UIApplication.shared.open(phoneCallURL, options: [:], completionHandler: nil)
-//            }
-//        }
-    }
-    
     func nextQuestion(qNbr: Int, tag: Int) {
         if qNbr == questions.count - 1 {
             correctLabel.text = "Победа!"
             correctLabel.textColor = .red
             hintLabel.text = questions[qNbr].hintA
             self.nextButton.setTitle("Начать сначала", for: .normal)
-            let btnArr = [btnA, btnB, btnC, btnD]
-            for btn in btnArr {
-                if btn?.tag == tag {
-                    btn?.backgroundColor = UIColor(red: 255/255, green: 83/255, blue: 13/255, alpha: 1)
-                    btn?.blink(enabled: true, duration: 0.5, stopAfter: 1.5)
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                        btn?.backgroundColor = UIColor(red: 4/255, green: 213/255, blue: 134/255, alpha: 1)
-                        self.afterAnswerView.isHidden = false
-                    }
-                }
+        } else {
+            correctLabel.text = "Правильно!"
+            switch tag {
+            case 0:
+                hintLabel.text = questions[qNbr].hintA
+            case 1:
+                hintLabel.text = questions[qNbr].hintB
+            case 2:
+                hintLabel.text = questions[qNbr].hintC
+            default:
+                hintLabel.text = questions[qNbr].hintD
             }
-            return
+            self.nextButton.setTitle("Продолжить", for: .normal)
         }
-        correctLabel.text = "Правильно!"
-        switch tag {
-        case 0:
-            hintLabel.text = questions[qNbr].hintA
-        case 1:
-            hintLabel.text = questions[qNbr].hintB
-        case 2:
-            hintLabel.text = questions[qNbr].hintC
-        default:
-            hintLabel.text = questions[qNbr].hintD
-        }
-        self.nextButton.setTitle("Продолжить", for: .normal)
-        let btnArr = [btnA, btnB, btnC, btnD]
-        for btn in btnArr {
-            if btn?.tag == tag {
-                btn?.backgroundColor = UIColor(red: 255/255, green: 83/255, blue: 13/255, alpha: 1)
-                btn?.blink(enabled: true, duration: 0.5, stopAfter: 1.5)
+        for btn in answerButtons {
+            if btn.tag == tag {
+                btn.backgroundColor = UIColor(red: 255/255, green: 83/255, blue: 13/255, alpha: 1)
+                btn.blink(enabled: true, duration: 0.5, stopAfter: 1.5)
                 DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                    btn?.backgroundColor = UIColor(red: 4/255, green: 213/255, blue: 134/255, alpha: 1)
+                    btn.backgroundColor = UIColor(red: 4/255, green: 213/255, blue: 134/255, alpha: 1)
                     self.afterAnswerView.isHidden = false
                 }
             }
         }
-/*
- зеленый
- 4
- 213
- 134
- 
- оранжевй
- 255
- 83
- 13
- 
- красный
- 255
- 29
- 32
-*/
     }
     
     func showIncorrectAlert(tag: Int) {
@@ -213,14 +213,12 @@ class ViewController: UIViewController {
             hintLabel.text = questions[qNbr].hintD
         }
         self.nextButton.setTitle("Начать сначала", for: .normal)
-       // afterAnswerView.isHidden = false
-        let btnArr = [btnA, btnB, btnC, btnD]
-        for btn in btnArr {
-            if btn?.tag == tag {
-                btn?.backgroundColor = UIColor(red: 255/255, green: 83/255, blue: 13/255, alpha: 1)
-                btn?.blink(enabled: true, duration: 0.5, stopAfter: 1.5)
+        for btn in answerButtons {
+            if btn.tag == tag {
+                btn.backgroundColor = UIColor(red: 255/255, green: 83/255, blue: 13/255, alpha: 1)
+                btn.blink(enabled: true, duration: 0.5, stopAfter: 1.5)
                 DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                    btn?.backgroundColor = UIColor(red: 255/255, green: 29/255, blue: 32/255, alpha: 1)
+                    btn.backgroundColor = UIColor(red: 255/255, green: 29/255, blue: 32/255, alpha: 1)
                     self.afterAnswerView.isHidden = false
                 }
             }
@@ -233,10 +231,9 @@ class ViewController: UIViewController {
         self.btnC.setTitle(self.questions[qNbr].ansC, for: .normal)
         self.btnD.setTitle(self.questions[qNbr].ansD, for: .normal)
         questionText.text = questions[qNbr].text
-        let btnArr = [btnA, btnB, btnC, btnD]
-        for btn in btnArr {
-            btn?.backgroundColor = UIColor(red: 2/255, green: 53/255, blue: 143/255, alpha: 1)
-            btn?.isEnabled = true
+        for btn in answerButtons {
+            btn.backgroundColor = UIColor(red: 2/255, green: 53/255, blue: 143/255, alpha: 1)
+            btn.isEnabled = true
         }
         if qNbr == 0 {
             fiftyIsUsed = 0
@@ -250,12 +247,22 @@ class ViewController: UIViewController {
             }
         }
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         AskView.isHidden = true
         afterAnswerView.isHidden = true
         showQuestionWithNbr(qNbr: 0)
-        
+        afterAnswerView.layer.cornerRadius = 10
+        AskView.layer.cornerRadius = 10
+        for btn in answerButtons {
+            btn.layer.cornerRadius = 5
+            btn.layer.masksToBounds = true
+        }
+//        answerButtons.forEach { (btn: UIButton) in
+//            btn.layer.cornerRadius = 5
+//            btn.layer.masksToBounds = true
+//        }
     }
 
 }
@@ -277,6 +284,23 @@ extension UIButton {
         }
     }
 }
+
+/*
+ зеленый
+ 4
+ 213
+ 134
+ 
+ оранжевй
+ 255
+ 83
+ 13
+ 
+ красный
+ 255
+ 29
+ 32
+ */
 
 //        let alert = UIAlertController(title:"Correct!", message: questions[qNbr].hint, preferredStyle: .alert)
 //        alert.addAction(UIAlertAction(title: "Next Question!", style: .default, handler: {
